da03c7ef3c9a5325621185e3da00d62a8dbd569c
SeddiMK <web.egorovm@gmail.com>
2023-01-03 17:22:48 +0300

    1_markup_Ok

--- js/files/script.js
+++ js/files/script.js
@@ -2,3 +2,22 @@
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
+//=============================================
+// Найти кнопки и отпуты
+// Взять данные из апи
+// Вставить данные в отпуты соответствующим данным
+// Привязать кнопку к реверсу данных...
+//=============================================
+//1. Получаем фай .json с кодировками валюты
+// fetch('http://www.floatrates.com/daily/usd.json').then(function (result) {
+// 	return result.json()
+// }).then(function (data) {
+// 	console.log(data);
+// })
+
+async function getCurrencies() {
+	const response = await fetch('http://www.floatrates.com/daily/usd.json');
+
+}
+
+// 2. 
